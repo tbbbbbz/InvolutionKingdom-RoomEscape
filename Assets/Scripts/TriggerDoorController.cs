76610f8ae20b874c4ea5f869433772f24b1affe7
Zou, Tengbo <tengbozou@gatech.edu>
2021-10-24 13:51:44 -0700

    Changed room (#15)
    * rebase master solving conflicts in scene
    
    * updated cat and character
    
    Co-authored-by: Tengbo Zou <tzou33@gatech.edu>

---
+++ Assets/Scripts/TriggerDoorController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor1 = null;
+    [SerializeField] private Animator myDoor2 = null;
+
+    [SerializeField] private bool openTrigger1 = false;
+    [SerializeField] private bool openTrigger2 = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (openTrigger1)
+            {
+                myDoor1.Play("OpenDoor1", 0, 0.0f);
+                myDoor2.Play("OpenDoor2", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+
+            else if (openTrigger2)
+            {
+                myDoor1.Play("OpenDoor11", 0, 0.0f);
+                myDoor2.Play("OpenDoor22", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
