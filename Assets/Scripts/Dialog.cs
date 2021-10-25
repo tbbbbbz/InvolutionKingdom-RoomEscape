ea0c10a613f7bdba7f7e9cc8593acb0713c48d9a
mao mao <ositowang@gmail.com>
2021-10-25 00:53:08 -0700

    add reusable dialog system and new font

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialog
+{
+
+    [TextArea(3,10 )]
+    public string[] sentences;
+    public string npcName;
+}
---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogText;
+    private Queue<string> _sentences;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialog dialog)
+    {
+        animator.SetBool("Dialog_Open", true);
+        nameText.text = dialog.npcName;
+        _sentences.Clear();
+        foreach (var sentence in dialog.sentences)
+        {
+            _sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N) &&
+            animator.GetBool("Dialog_Open"))
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void DisplayNextSentence()
+    {
+        Debug.Log("Display Next Sentence");
+        if (_sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+
+        string sentence = _sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(WriteSentence(sentence));
+    }
+
+    IEnumerator WriteSentence(string sentence)
+    {
+        dialogText.text = "";
+        foreach (var letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+    }
+
+    private void EndDialog()
+    {
+        animator.SetBool("Dialog_Open", false);
+    }
+
+    // Update is called once per frame
+}
