c930f86127a7a1fc7f4153e8ef38a3996ee4f603
FloraZhong <fzhong8@gatech.edu>
2021-11-02 19:44:44 -0700

    Add sounds and modify dialog UI
    Add sounds and modify dialog UI

---
+++ Assets/Scripts/AppEvents/AudioEvent/BasketCollisionReporter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketCollisionReporter : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision c)
+    {
+
+        if (c.impulse.magnitude > 0.25f)
+        {
+            //we'll just use the first contact point for simplicity
+            EventManager.TriggerEvent<CollideBasketEvent, Vector3, float>(c.contacts[0].point, c.impulse.magnitude);
+        }
+
+
+        //				foreach (ContactPoint contact in c.contacts) {
+        //
+        //						if (c.impulse.magnitude > 0.5f)
+        //								EventManager.TriggerEvent<AudioEventManager.CollideDeskChairEvent, Vector3> (contact.point);
+        //						
+        //				}
+    }
+}
