4af8ec07e72560363d4f44cdd9278c45780f00a7
Tengbo Zou <tzou33@gatech.edu>
2021-11-08 20:51:01 -0800

    added drawer panel trigger

---
+++ Assets/Scripts/Interactables/DrawersControlPanel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawersControlPanel : InteractableObject
+{
+    private GameObject canvas;
+
+    protected void Start()
+    {
+        base.Start();
+        canvas = this.gameObject.transform.Find("Canvas").gameObject;
+    }
+
+    private void Update()
+    {
+        if (playerNearBy && Input.GetKeyDown(KeyCode.O))
+        {
+            canvas.SetActive(true);
+        }
+    }
+
+    protected void OnTriggerExit(Collider other)
+    {
+        base.OnTriggerExit(other);
+        canvas.SetActive(false);
+    }
+}
